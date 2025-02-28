1c774807f1800708f8dfef50a1b6f1b41fff7007
Guluuuuuuuu <165867736+Guluuuuuuuu@users.noreply.github.com>
2025-03-01 00:01:55 +0800

    Initial commit

---
+++ src/main/java/com/gulu/internship07/Internship07Application.java
@@ -0,0 +1,13 @@
+package com.gulu.internship07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Internship07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Internship07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gulu/internship07/Internship07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulu.internship07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Internship07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
